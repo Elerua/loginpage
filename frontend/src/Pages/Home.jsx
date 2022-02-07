659cd285ea607e157ac74328f3370c2dd2e92c7f
Elerua <aurele.duparc@free.fr>
2022-02-07 01:16:06 +0100

    Changement de display

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -7,7 +7,7 @@ import '../CSS/Home.css';
 const Home = () => {
 	const buttonData = [
 		{
-            type: 'login',
+			type: 'login',
 			title: 'Se connecter',
 			path: '/',
 			icon: '',
@@ -15,7 +15,7 @@ const Home = () => {
 				"Si vous possédez déjà une adresse mail enregistrée ainsi qu'un mot de passe vous pouvez utiliser le bouton placé ci-dessous afin d'accéder à vos informations.",
 		},
 		{
-            type: 'signup',
+			type: 'signup',
 			title: "S'inscrire",
 			path: '/',
 			icon: '',
@@ -25,37 +25,16 @@ const Home = () => {
 	];
 	return (
 		<div className="home">
-			<div className="title">Authentification</div>
-			<div className="subtitle">
-				Cette page unique est une application utilisant ReactJS, .NET et MySQL afin
-				de se connecter à une application fictive et enregistrer des utilisateurs.
-			</div>
-			<div className="button-group">
-				{buttonData.map((item, index) => (
-					<div className={item.type} key={index}>
-                        <div className="button-title">{item.title}</div>
-                        <div className="button-explication">{item.explication}</div> 
-                        <div className="button-button"></div>
-					</div>
-				))}
-				{/*<div className="login">
-					<div className="button-title">Se connecter</div>
-					<div className="button-explication">
-						Si vous possédez déjà une adresse mail enregistrée ainsi qu'un mot de
-						passe vous pouvez utiliser le bouton placé ci-dessous afin d'accéder à vos
-						informations.
-					</div>
-					<div className="button"></div>
+			<div className="left">
+				<div className="left-title">Page de connexion</div>
+				<div className="left-text">
+					Cette page unique est une application utilisant ReactJS, .NET et MySQL afin
+					de se connecter à une application fictive et enregistrer des utilisateurs.
 				</div>
-				<div className="signup">
-					<div className="button-title"></div>
-					<div className="button-explication">
-						Si vous ne possédez pas d'adresse mail enregistrée ainsi qu'un mot de
-						passe vous pouvez utiliser le bouton placé ci-dessous afin de vous
-						enregistrer sur cette application.
-					</div>
-					<div className="button">S'inscrire'</div>
-                </div>*/}
+			</div>
+			<div className="right">
+				<div className="login">Login</div>
+				<div className="signup">Signup</div>
 			</div>
 		</div>
 	);
